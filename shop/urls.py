ef96e23d5572ff7894da2e82f358ceeb30a2ab5a
Anastac1o <duarte_anastacio15@hotmail.com>
2020-04-21 19:27:28 +0100

    work on template and removal of not necessary files

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.HomePageView.as_view(), name='home'),
     path('bootstrap', TemplateView.as_view(template_name='bootstrap/index.html')),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from django.views import generic
 from django.template import loader
 from .models import Item
-from django.views.generic import CreateView
+from django.views.generic import TemplateView, DetailView
 
 
 def index(request):
-    img = Item.objects.all().order_by('id')
     return render(request, 'shop/index.html', {"img":img})
 
-class CreateViewItem(CreateView):
-    model = Item
-    fields = ('name', 'author', 'is_rare', 'is_first_edition', 'is_used')
+
+class HomePageView(TemplateView):
+    template_name = "bootstrap/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_items'] = Items.objects.all()
+        return context
+
