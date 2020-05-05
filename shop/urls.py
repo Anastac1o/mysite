f9efb3c622ebec2376914e3c99f710e3b5e92da2
Anastac1o <duarte_anastacio15@hotmail.com>
2020-05-05 07:20:28 +0100

    Several unecessary files deleted, managed to display Images from Item Model

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.conf import settings # new
+from django.urls import path, include # new
+from django.conf.urls.static import static # new
+
 
 app_name = 'shop'
 
@@ -8,3 +12,5 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('bootstrap', TemplateView.as_view(template_name='bootstrap/index.html')),
 ]
+
+
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from django.template import loader
+
+
 from .models import Item
 from django.views.generic import TemplateView, DetailView
-
-
-def index(request):
-    return render(request, 'shop/index.html', {"img":img})
 
 
 class HomePageView(TemplateView):
@@ -14,6 +9,6 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['all_items'] = Items.objects.all()
+        context['all_items'] = Item.objects.all()
         return context
 
